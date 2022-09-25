6b6ebab30882aca187dd4c18819d1ee3b4b634f9
Kannan Jayachandran <kannanjayachandran16@gmail.com>
2022-09-25 14:24:38 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
+const slot_algo = () => {
+  let x = '👑';
+  let y = '👑';
+  let z = '✅';
+
+  if (x === y && y === z) {
+    return (
+      <>
+        <div className="slot_won">
+          <h1> {x} {y} {z} </h1>
+          <hr />
+          <h1>🎉 You won 🎉</h1>
+        </div>
+      </>
+    );
+  } else {
+
+    return (
+      <>
+        <div className="slot_won">
+          <h1> {x} {y} {z} </h1>
+          <hr />
+          <h1>Not Matching</h1>
+        </div>
+      </>
+    );
+
+  }
+};
+
+
 const App = () => {
 
   return <>
     <h1 className="heading-style"> 🎰 Slot machine 🎰 </h1>
 
-
-
+    <slot_algo />
 
   </>;
 };
-
-
-
-
-
 
 export default App;
